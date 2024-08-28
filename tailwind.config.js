9abbc835f3699e5c380ebefa34c1b0f44c10cc1f
SpotCrux <uchihanadare0075@gmail.com>
2024-08-28 02:19:33 +0000

    Hero edited

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./src/**/*.{html,js,njk}"],
   theme: {
-    extend: {},
+    
+    extend: {
+      screens: {
+      'xs':'100px',
+      'sm':'640px',
+      'md':'768px',
+      'lg':'1024px',
+      'xl':'1280px',
+      '2xl':'1536px',
+    },
+  },
   },
   plugins: [],
 }
